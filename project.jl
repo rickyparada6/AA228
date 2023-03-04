e26fc28b2f8e948194a49f9afd4c9be3a04b2d2f
rickyparada6 <32522794+rickyparada6@users.noreply.github.com>
2023-03-03 19:12:56 -0800

    minimum working pomdp

--- project.jl
+++ project.jl
@@ -1,65 +1,50 @@
-using Pkg; Pkg.add("POMDPs"); Pkg.add("QuickPOMDPs"); Pkg.add("POMDPModelTools"); Pkg.add("POMDPSimulators"); Pkg.add("QMDP")
-using POMDPs, QuickPOMDPs, POMDPModelTools, POMDPSimulators, QMDP
-using Printf
-using CSV
-using DataFrames
-using LinearAlgebra
+# using Pkg; Pkg.add("POMDPs"); Pkg.add("QuickPOMDPs"); Pkg.add("POMDPSimulators"); Pkg.add("QMDP")
+using POMDPs, QuickPOMDPs, POMDPSimulators, QMDP
+using POMDPTools: Deterministic, Uniform
+using Random
 
-function compute()
-
-end
-
-# tiger example
-m = QuickPOMDP(
-    states = ["left", "right"],
-    actions = ["left", "right", "listen"],
-    observations = ["left", "right"],
-    initialstate = Uniform(["left", "right"]),
+satellite = QuickPOMDP(
+    states = [(P, S1, E, O, T) for P in 0:100, S1 in 0:100, E in 0:1, O in 0:1, T in 0:4],
+    actions = [0, 1],
+    observations = [(P, S1, E, O, T) for P in 0:100, S1 in 0:100, E in 0:1, O in 0:1, T in 0:4],
+    initialstate = Deterministic((100,100,1,1,rand(0:4))),
     discount = 0.95,
 
     transition = function (s, a)
-        if a == "listen"
-            return Deterministic(s) # tiger stays behind the same door
-        else # a door is opened
-            return Uniform(["left", "right"]) # reset
+        if s == 0
+            return Deterministic(s)
+        elseif s[1] != 0
+            return Deterministic((s[1] - rand(1:s[1]),s[2],s[3],s[4],s[5]))
+        else
+            return Deterministic(s)
         end
     end,
 
     observation = function (s, a, sp)
-        if a == "listen"
-            if sp == "left"
-                return SparseCat(["left", "right"], [0.85, 0.15]) # sparse categorical distribution
-            else
-                return SparseCat(["right", "left"], [0.85, 0.15])
-            end
-        else
-            return Uniform(["left", "right"])
-        end
+        return Deterministic(s)
     end,
 
     reward = function (s, a)
-        if a == "listen"
-            return -1.0
-        elseif s == a # the tiger was found
-            return -100.0
-        else # the tiger was escaped
-            return 10.0
+        if s[3] == 1 && s[4] == 1 # functional satellite
+            r = 10
+        elseif s[3] == 1 || s[4] == 1 # partially functional
+            r = 0
+        else # broken
+            r = -100
         end
-    end
+    end,
+
+    isterminal = s -> s[1] == 0
 )
 
 solver = QMDPSolver()
-policy = solve(solver, m)
+policy = solve(solver, satellite)
 
 rsum = 0.0
-for (s,b,a,o,r) in stepthrough(m, policy, "s,b,a,o,r", max_steps=10)
-    println("s: $s, b: $([s=>pdf(b,s) for s in states(m)]), a: $a, o: $o")
+for (s,a,o,r) in stepthrough(satellite, policy, "s,a,o,r", max_steps=10)
+    println("in state $s")
+    println("took action $a")
+    println("received observation $o and reward $r")
     global rsum += r
 end
 println("Undiscounted reward was $rsum.")
-
-if length(ARGS) != 0
-    error("usage: julia project.jl")
-end
-
-compute()
